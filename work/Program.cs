3f88cf3afb0bee4a24041b3476d261814cbf565a
mate bregvadze <mate.bregvadze@gmail.com>
2023-05-06 18:36:43 +0400

    homework 2

---
+++ work/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("mate bregvadze");
+            Console.WriteLine("enter your username");
+            var username = Console.ReadLine();           
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("your user name is " + username);
+
+        }
+    }
+}
